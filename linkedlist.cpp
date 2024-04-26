b8bd3c6cb8e3738ef8dda5ab22caefe6e2c0b79b
rnktlhr <rinakitluhur10@gmail.com>
2024-04-26 14:51:08 +0700

    menambahkan void traverse dan if di dalamnya

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -92,3 +92,13 @@ bool listEmpty() {
     else
         return false;
 }
+
+void traverse() {
+    if (listEmpty()) {
+        cout << "List Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+    
+}
