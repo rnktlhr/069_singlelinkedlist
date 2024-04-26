bdab08ccef499d51ed317f5652a950ed4307d778
rnktlhr <rinakitluhur10@gmail.com>
2024-04-26 14:55:31 +0700

    menambahkan else pada void traverse

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -100,5 +100,11 @@ void traverse() {
         system("cls");
         return;
     }
-    
+    else{
+        Node* currentNode = START;
+        while(currentNode != NULL){
+            cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }
 }
